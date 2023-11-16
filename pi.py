d8b0b2365cf87595a06c5b355eaa9655d7cd6551
SHO <s.hatsuki.23.10.1210@4nonome.com>
2023-11-16 22:59:46 +0900

    python入門問1

--- pi.py
+++ pi.py
@@ -5,3 +5,9 @@ text = """
 
 # TODO
 
+nList = list(map(len, ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving",
+"quantum", "mechanics", "All", "of", "thy", "geometry", "Herr Planck", "is", "fairly", "hard",]))
+print(nList)
+
+n = int("".join(map(str, nList)))
+print(n)
