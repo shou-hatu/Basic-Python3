06251c88be8df09172a9a50388287bff1a73643f
SHO <s.hatsuki.23.10.1210@4nonome.com>
2023-11-21 18:18:34 +0900

    制御構文課題

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,30 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+number = 61
+count = 2
+while True:
+    number % count 
+    count = count + 1   
+    if number % count == 0:
+        print('{}は素数でない'.format(number))
+        break
+    elif  count > number ** (1/2):
+        print('{}は素数'.format(number))
+        break
+
+number = 10
+count = 2
+while count <= number:
+    number % count 
+    count = count + 1   
+    print(number % count)
+    if number % count == 0:
+        print('{}は素数でない'.format(number))
+        break
+    elif  count > number ** (1/2):
+        print('{}は素数'.format(number))
+        break
+
+     
+
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,20 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+import math
+h = ( math.pi*(1/2)- 0)/ 100
+count = 1
+
+#while文で初項から100項までリストに入れる
+while count <= 100:
+    terms = []
+    terms.append((h / 2)*((sin(0 + (count - 1)* h) + sin(0 + count * h))))
+ #リストに入れた項の総和を出す
+def sygma(terms):
+    s = 0
+    for i in range (0, len(terms)):
+        s = s + terms[i]
+        return s
+print(sygma(terms))
+    
+        
