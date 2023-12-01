edc17a54c2f62be45a416266b5d8511f5d52bb72
SHO <s.hatsuki.23.10.1210@4nonome.com>
2023-12-01 18:18:57 +0900

    素数判定修正

--- prime_number.py
+++ prime_number.py
@@ -4,29 +4,24 @@ b = input("bの値を入力: ")
 # TODO
 #出力された後にコロンの後に数字を入力できる→入力した数値に対して素数判定できるようにするといい
 #数値を入力した際にその値を素数判定させるイメージ
-firstnumber = int(a)
-
-for i in range(2,firstnumber) :
-    firstnumber % i 
-    i = i + 1   
-    if firstnumber % i == 0:
-        print('{}は素数でない'.format(firstnumber))
-        break
-    elif  i == (firstnumber - 1):
-        print('{}は素数'.format(firstnumber))
-        break
 
 
-secondnumber = int(b)
-for i in range(2,secondnumber) :
-    secondnumber % i 
-    i = i + 1   
-    if secondnumber % i == 0:
-        print('{}は素数でない'.format(secondnumber))
-        break
-    elif  i == (secondnumber - 1):
-        print('{}は素数'.format(secondnumber))
-        break
+def  primalitytest(number):
+    if  number == 1:
+        return('{}は素数でない'.format(number))
+    if  number == 2:
+        return('{}は素数'.format(number))    
+    for i in range(2,(number + 1)) :
+        number % i   
+        if number % i == 0:
+                return('{}は素数でない'.format(number))
+                
+        elif  i == (number - 1):
+                return('{}は素数'.format(number))
+print(primalitytest(int(a)))
+print(primalitytest(int(b)))
+                
 
      
+
 
