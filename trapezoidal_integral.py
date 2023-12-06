8ea500d92300b5a6f276c761ae4fb7df4bccc76f
SHO <s.hatsuki.23.10.1210@4nonome.com>
2023-12-06 14:52:50 +0900

    台形積分修正

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -6,38 +6,21 @@ from math import sin
 import math
 
 #(1)
-def ffirst(x):
-    return (sin(x))
-def trintegral(a = 0,b = 1,n = 100):
+def trintegral(f,a = 0,b = 1,n = 100):
+
     h = (b - a) / n
     count = 1
     terms = 0
     for count in range(1,(n + 1)):   
-        term = (h / 2)*((ffirst(a + (count - 1)* h) + ffirst(a + count * h)))
+        term = (h / 2)*((f(a + (count - 1)* h) + f(a + count * h)))
         terms = terms + term
     return(terms)
-print('(1){}'.format(trintegral(0,((math.pi) / 2),50)))
+print('(1){}'.format(trintegral(sin,0,((math.pi) / 2),50)))
 #(2)
 def fsecond(x):
     return 4/(1 + (x**2))
-def trintegral(a = 0,b = 1,n = 100):
-    h = (b - a) / n
-    count = 1
-    terms = 0
-    for count in range(1,(n + 1)):   
-        term = (h / 2)*((fsecond(a + (count - 1)* h) + fsecond(a + count * h)))
-        terms = terms + term
-    return(terms)
-print('(2){}'.format(trintegral(0,1,100)))
+print('(2){}'.format(trintegral(fsecond,0,1,100)))
 #(3)
 def fthird(x):
     return math.pi ** (1 / 2) * math.exp(- x ** 2)
-def trintegral(a = 0,b = 1,n = 100):
-    h = (b - a) / n
-    count = 1
-    terms = 0
-    for count in range(1,(n + 1)):   
-        term = (h / 2)*((fthird(a + (count - 1)* h) + fthird(a + count * h)))
-        terms = terms + term
-    return(terms)
-print('(3){}'.format(trintegral(-100,100,1000)))
+print('(3){}'.format(trintegral(fthird,-100,100,1000)))
